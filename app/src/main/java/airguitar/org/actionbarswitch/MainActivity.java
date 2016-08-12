829f399d0fbccdc027e76a7aee82ab1fec74775c
akihiko <akihiko@airguitar.org>
2016-08-12 18:02:32 +0900

    Initial commit

---
+++ app/src/main/java/airguitar/org/actionbarswitch/MainActivity.java
@@ -0,0 +1,33 @@
+package airguitar.org.actionbarswitch;
+
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.switch_menu, menu);
+        MenuItem switchMenu = menu.findItem(R.id.allSwitch);
+        Switch switchView =(Switch) MenuItemCompat.getActionView(switchMenu);
+        switchView.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Start or stop your Service
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+}
